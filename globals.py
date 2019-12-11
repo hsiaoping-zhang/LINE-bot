aa12d0fd84931e38023715bb979213f4978c5549
hsiaoping-zhang <47858205+hsiaoping-zhang@users.noreply.github.com>
2019-12-11 10:59:11 +0800

    Delete globals.py

--- globals.py
+++
@@ -1,6 +0,0 @@
-def initialize(): 
-	global db 
-	cred = credentials.Certificate('./linebot-helper-firebase-adminsdk-bojx6-d085b09386.json') 
-	firebase_admin.initialize_app(cred)
-	db = firestore.client()
-	print("enter initialize function")
